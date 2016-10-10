ccfbf5fdbe9a7c8d79c9f009338bf42c66a750cf
graphicsxp <graphicsxp@gmail.com>
2016-10-10 21:09:49 +0200

    cordova

--- src/pages/orderForm/component/orderForm-list.component.ts
+++ src/pages/orderForm/component/orderForm-list.component.ts
@@ -4,6 +4,8 @@ import { NavController, ModalController } from 'ionic-angular';
 import { OrderFormDetailComponent } from './orderForm-detail.component';
 import { OrderFormService } from '../service/orderForm-service';
 import { LoadingService } from '../../shared/service/loading-service';
+import { Vibration, Badge, Transfer, File, FileOpener } from 'ionic-native';
+
 //import * as  _ from 'lodash';
 
 
@@ -33,5 +35,27 @@ export class OrderFormListComponent
         this._navCtrl.push(OrderFormDetailComponent, {id: item.id})
         //let modal = this._modalCtrl.create(OrderFormDetailComponent, item);
         //modal.present();
+    }
+
+    openOrderForm(item: IOrderForm): void {
+        console.log('clicking on icon');
+        
+        // let fileTransfer: Transfer = new Transfer();
+        // // var cordova: any;
+        // // const fs:string = cordova.file.dataDirectory;
+        // let targetPath = cordova.file.externalDataDirectory + 'myOrderForm.pdf';
+
+        // fileTransfer.download('http://www.sam-dev.net/wp-content/uploads/doc.pdf', targetPath).then((res) => {
+        //     console.log('the file was downloaded successfully:' + res);
+        //     FileOpener.open(targetPath, 'application/pdf').then((res) => {
+        //         console.log('the file was opened successfully:' + res);
+        //     }).catch(err => {
+        //         console.log('an error occured while opening the file:' + err)
+        //     });
+        //     Vibration.vibrate(1500);
+        // }).catch((err) => {
+        //     console.log('an error occured while downloading the file:' + err)
+        //     Vibration.vibrate(100);
+        // });
     }
 }
---
+++ typings/index.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="globals/cordova/index.d.ts" />
+/// <reference path="globals/cordova/plugins/filesystem/index.d.ts" />
+/// <reference path="globals/cordova/plugins/filetransfer/index.d.ts" />
