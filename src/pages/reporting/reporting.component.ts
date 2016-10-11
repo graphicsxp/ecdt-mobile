13873b7b0bfced595fa4c85467c82114bee40a06
graphicsxp <graphicsx@gmail.com>
2016-10-11 11:16:19 +0200

    added dropdown to select chart type

--- src/pages/reporting/reporting.component.ts
+++ src/pages/reporting/reporting.component.ts
@@ -8,17 +8,17 @@ import { NavController } from 'ionic-angular';
 })
 export class ReportingComponent {
 
-  public lineChartData: Array<any> = [
+  public chartData: Array<any> = [
     { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
     { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
     { data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C' }
   ];
-  public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartOptions: any = {
+  public chartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public chartOptions: any = {
     animation: false,
     responsive: true
   };
-  public lineChartColors: Array<any> = [
+  public chartColors: Array<any> = [
     { 
       backgroundColor: 'rgba(255,159,177,0.2)',
       borderColor: 'rgba(255,159,177,1)',
@@ -44,8 +44,8 @@ export class ReportingComponent {
       pointHoverBorderColor: 'rgba(148,159,177,0.8)'
     }
   ];
-  public lineChartLegend: boolean = true;
-  public lineChartType: string = 'line';
+  public chartLegend: boolean = true;
+  public chartType: string = 'line';
 
   options: Object;
 
