a09c9f76dcc2778530a816fade72360746a28901
lnu <l.nullens@gmail.com>
2016-10-23 18:31:01 +0200

    Use platform ready before using plugin

--- src/pages/shared/service/networkAvailability-service.ts
+++ src/pages/shared/service/networkAvailability-service.ts
@@ -1,3 +1,4 @@
+import { Platform } from 'ionic-angular';
 import { Injectable } from '@angular/core';
 import { Network } from 'ionic-native';
 import { LoadingService } from './loading-service';
@@ -8,28 +9,30 @@ export class NetworkAvailabilityService {
   private _connectSubscription: any;
   private _connected: boolean;
 
-  constructor(loadingService: LoadingService) {
-    this._connected = this.networkAvailable();
-    console.log('subscribing to network events');
-    this._disconnectSubscription = Network.onDisconnect().subscribe(() => {
-      console.log('network disconnected:' + Network.connection);
-      setTimeout(() => {
-        if (Network.connection === 'none' && this._connected === true) {
-          this._connected = false;
-          console.log('network disconnected handled:' + Network.connection);
-          loadingService.presentNetworkNotAvailableLoading();
-        }
-      }, 1500);
-    });
-    this._connectSubscription = Network.onConnect().subscribe(() => {
-      console.log('network connected:' + Network.connection);
-      setTimeout(() => {
-        if (Network.connection !== 'none' && this._connected === false) {
-          this._connected = true;
-          console.log('network connected handled:' + Network.connection);
-          loadingService.hideNetworkLoading();
-        }
-      }, 1500);
+  constructor(platform:Platform, loadingService: LoadingService) {
+    platform.ready().then(() => {
+      this._connected = this.networkAvailable();
+      console.log('subscribing to network events');
+      this._disconnectSubscription = Network.onDisconnect().subscribe(() => {
+        console.log('network disconnected:' + Network.connection);
+        setTimeout(() => {
+          if (Network.connection === 'none' && this._connected === true) {
+            this._connected = false;
+            console.log('network disconnected handled:' + Network.connection);
+            loadingService.presentNetworkNotAvailableLoading();
+          }
+        }, 1500);
+      });
+      this._connectSubscription = Network.onConnect().subscribe(() => {
+        console.log('network connected:' + Network.connection);
+        setTimeout(() => {
+          if (Network.connection !== 'none' && this._connected === false) {
+            this._connected = true;
+            console.log('network connected handled:' + Network.connection);
+            loadingService.hideNetworkLoading();
+          }
+        }, 1500);
+      });
     });
   }
 
