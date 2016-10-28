e3a047ee7c08c0b0555b0743e019d604e5516a4d
graphicsxp <graphicsx@gmail.com>
2016-10-28 10:06:39 +0200

    rolled back to rollup added named export to rollup config debug -> ok kendo prod -> ok

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -38,9 +38,37 @@ var rollupConfig = {
   plugins: [
     builtins(),
     commonjs({
-        namedExports: {
-            '@progress/kendo-angular-charts': ['ChartsModule']
-        }
+      namedExports: {
+        '@progress/kendo-angular-charts': ['ChartsModule'],
+        '@progress/kendo-angular-intl/dist/npm/js/intl.service': ['IntlService'],
+        '@progress/kendo-angular-intl/dist/npm/js/cldr-intl.service': ['CldrIntlService'],
+        '@progress/kendo-angular-popup/dist/npm/js/popup.module': ["PopupModule"],
+        '@progress/kendo-angular-charts/dist/npm/js/charts.module': ['ChartsModule'],
+
+        '@progress/kendo-angular-charts/dist/npm/js/common/theme.service': ['ThemeService'],
+        '@progress/kendo-angular-charts/dist/npm/js/chart.component': ['ChartComponent'],
+        '@progress/kendo-angular-charts/dist/npm/js/common/tooltip-template.service': ['TooltipTemplateService'],
+        '@progress/kendo-angular-charts/dist/npm/js/chart/chart-auto-theme.component': ['ChartAutoThemeComponent'],
+        '@progress/kendo-angular-charts/dist/npm/js/common/configuration.service' :  ['ConfigurationService'],
+        '@progress/kendo-angular-charts/dist/npm/js/common/collection.service' :  ['CollectionService'],
+        '@progress/kendo-angular-charts/dist/npm/js/chart/value-axis.component': ['ValueAxisComponent'],
+        '@progress/kendo-angular-charts/dist/npm/js/chart/value-axis-item.component': ['ValueAxisItemComponent'],
+        '@progress/kendo-angular-charts/dist/npm/js/chart/category-axis-item.component': ['CategoryAxisItemComponent'],
+        '@progress/kendo-angular-charts/dist/npm/js/chart/category-axis.component': ['CategoryAxisComponent'],
+        '@progress/kendo-angular-charts/dist/npm/js/chart/series.component': ['SeriesComponent'],
+        '@progress/kendo-angular-charts/dist/npm/js/chart/series-item.component': ['SeriesItemComponent'],
+        '@progress/kendo-angular-popup/dist/npm/js/services/position.service': ['PositionService'],
+        '@progress/kendo-angular-popup/dist/npm/js/services/align.service': ['AlignService'],
+        '@progress/kendo-angular-popup/dist/npm/js/popup.component': ['PopupComponent'],
+        '@progress/kendo-angular-popup/dist/npm/js/services/dom.service': ['DOMService'],
+        '@progress/kendo-angular-charts/dist/npm/js/chart/tooltip/crosshair-tooltips.component': ['CrosshairTooltipsComponent'],
+        '@progress/kendo-angular-charts/dist/npm/js/chart/tooltip/crosshair-tooltip.component': ['CrosshairTooltipComponent'],
+        '@progress/kendo-angular-charts/dist/npm/js/chart/tooltip/crosshair-tooltips-container.component': ['CrosshairTooltipsContainerComponent'],
+        '@progress/kendo-angular-charts/dist/npm/js/chart/tooltip/tooltip-popup.component': ['TooltipPopupComponent'],
+        '@progress/kendo-angular-charts/dist/npm/js/chart/tooltip/shared-tooltip-template.directive': ['SharedTooltipTemplateDirective'],        
+        '@progress/kendo-angular-charts/dist/npm/js/chart/tooltip/series-tooltip-template.directive': ['SeriesTooltipTemplateDirective'],        
+        
+      }
     }),
     nodeResolve({
       module: true,
