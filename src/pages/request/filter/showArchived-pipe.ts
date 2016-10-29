e1029f4e0a8c97cda2496c276a65c6758887ab62
Laurent Nullens <l.nullens@gmail.com>
2016-10-29 11:54:41 +0200

    Package update ShowArchivedPipe updated(was not working in prod) Missing typings(removed previously by error) Works with webpack, missing exports with rollup

--- src/pages/request/filter/showArchived-pipe.ts
+++ src/pages/request/filter/showArchived-pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 
 export class ShowArchivedPipe implements PipeTransform {
-    transform(value: any, args: any[]): any {
+    transform(value: any, args: boolean): string {
         return value.filter(item => !item.isArchived || item.isArchived === args);
     }
 }
