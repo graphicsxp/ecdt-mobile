0bf9cb1acdf5aa56cd974c297cac1d5fb1021526
Laurent Nullens <l.nullens@gmail.com>
2016-11-03 10:02:29 +0100

    Webpack config(copied from app-script config)

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var webpack = require('webpack');
 
 // for prod builds, we have already done AoT and AoT writes to disk
 // so read the JS file from disk
@@ -9,20 +10,49 @@ function getEntryPoint() {
   if (process.env.IONIC_ENV === 'prod') {
     return '{{TMP}}/app/main.prod.js';
   }
-  return '{{SRC}}/app/main.dev.ts';
+  return '{{TMP}}/app/main.dev.js';
+}
+
+function getPlugins() {
+  if (process.env.IONIC_ENV === 'prod') {
+    return [
+      // This helps ensure the builds are consistent if source hasn't changed:
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      // Try to dedupe duplicated modules, if any:
+      // Add this back in when Angular fixes the issue: https://github.com/angular/angular-cli/issues/1587
+      //new DedupePlugin()
+    ];
+  }
+  return [];
 }
 
 module.exports = {
-  devtool: 'source-map',
   entry: getEntryPoint(),
   output: {
     path: '{{BUILD}}',
-    devtoolModuleFilenameTemplate : '[resource-path]',
-    filename: 'main.js',
+    filename: 'main.js'
+  },
+
+  resolve: {
+    extensions: ['.js', '.json']
   },
 
   module: {
     loaders: [
+      {
+        test: /\.json$/,
+        loader: 'json'
+      }
     ]
+  },
+
+  plugins: getPlugins(),
+
+  // Some libraries import Node modules but don't use them in the browser.
+  // Tell Webpack to provide empty mocks for them so importing them works.
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
   }
 };
