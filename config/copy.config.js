a02083e20a3e56c354951f43df579e4672a3dec7
lnu <l.nullens@gmail.com>
2017-11-11 16:34:56 +0100

    ionic app scripts updated

--- config/copy.config.js
+++ config/copy.config.js
@@ -2,28 +2,32 @@
 // provide a name for an entry, it can be anything such as 'copyAssets' or 'copyFonts'
 // then provide an object with a `src` array of globs and a `dest` string
 module.exports = {
-  copyAssets: {
-    src: ['{{SRC}}/assets/**/*'],
-    dest: '{{WWW}}/assets'
-  },
-  copyIndexContent: {
-    src: ['{{SRC}}/index.html', '{{SRC}}/manifest.json', '{{SRC}}/service-worker.js'],
-    dest: '{{WWW}}'
-  },
-  copyFonts: {
-    src: ['{{ROOT}}/node_modules/ionicons/dist/fonts/**/*', '{{ROOT}}/node_modules/ionic-angular/fonts/**/*'],
-    dest: '{{WWW}}/assets/fonts'
-  },
-  copyPolyfills: {
-    src: ['{{ROOT}}/node_modules/ionic-angular/polyfills/polyfills.js'],
-    dest: '{{BUILD}}'
-  },
-   copyJson: {
-    src: ['{{SRC}}/api/orderForms','{{SRC}}/api/reporting/stacked-chart.json','{{SRC}}/api/reporting/simple-chart.json','{{SRC}}/api/requests'],
-    dest: '{{BUILD}}'
-  },
-  copyTelerik:{
-    src: ['{{ROOT}}/node_modules/@telerik/kendo-theme-default/dist/all.css'],
-    dest: '{{BUILD}}'
-  }
+    copyAssets: {
+        src: ['{{SRC}}/assets/**/*'],
+        dest: '{{WWW}}/assets'
+    },
+    copyIndexContent: {
+        src: ['{{SRC}}/index.html', '{{SRC}}/manifest.json', '{{SRC}}/service-worker.js'],
+        dest: '{{WWW}}'
+    },
+    copyFonts: {
+        src: ['{{ROOT}}/node_modules/ionicons/dist/fonts/**/*', '{{ROOT}}/node_modules/ionic-angular/fonts/**/*'],
+        dest: '{{WWW}}/assets/fonts'
+    },
+    copyPolyfills: {
+        src: ['{{ROOT}}/node_modules/ionic-angular/polyfills/polyfills.js'],
+        dest: '{{BUILD}}'
+    },
+    copySwToolbox: {
+        src: ['{{ROOT}}/node_modules/sw-toolbox/sw-toolbox.js'],
+        dest: '{{BUILD}}'
+    },
+    copyJson: {
+        src: ['{{SRC}}/api/orderForms', '{{SRC}}/api/reporting/stacked-chart.json', '{{SRC}}/api/reporting/simple-chart.json', '{{SRC}}/api/requests'],
+        dest: '{{BUILD}}'
+    },
+    copyTelerik: {
+        src: ['{{ROOT}}/node_modules/@telerik/kendo-theme-default/dist/all.css'],
+        dest: '{{BUILD}}'
+    }
 };
